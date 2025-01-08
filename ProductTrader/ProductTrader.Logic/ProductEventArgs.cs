21886c1b505f62977e08f99c536debbee319b46e
Enki <cell.enohp.apps@gmail.com>
2025-01-08 16:35:39 +0100

    added comment to everything

--- ProductTrader/ProductTrader.Logic/ProductEventArgs.cs
+++ ProductTrader/ProductTrader.Logic/ProductEventArgs.cs
@@ -2,7 +2,15 @@
 
 public class ProductEventArgs : EventArgs
 {
-        #region CONSTRUCTORS
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductEventArgs"/> class.
+        /// </summary>
+        /// <param name="name">The name of the product.</param>
+        /// <param name="value">The current value of the product.</param>
+        /// <param name="minValue">The minimum allowed value for the product.</param>
+        /// <param name="maxValue">The maximum allowed value for the product.</param>
         public ProductEventArgs(string name , double value , double minValue , double maxValue)
         {
                 Name = name;
@@ -10,12 +18,30 @@ public class ProductEventArgs : EventArgs
                 MinValue = minValue;
                 MaxValue = maxValue;
         }
+
         #endregion
 
         #region PROPERTIES
+
+        /// <summary>
+        /// Gets the name of the product.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// Gets the current value of the product.
+        /// </summary>
         public double Value { get; }
+
+        /// <summary>
+        /// Gets the minimum allowed value for the product.
+        /// </summary>
         public double MinValue { get; }
+
+        /// <summary>
+        /// Gets the maximum allowed value for the product.
+        /// </summary>
         public double MaxValue { get; }
+
         #endregion
 }
--- ProductTrader/ProductTrader.Logic/Trader.cs
+++ ProductTrader/ProductTrader.Logic/Trader.cs
@@ -3,28 +3,72 @@
 public class Trader : ITrader
 {
         #region FIELDS
-        private bool _hasBought = false;
-        private double _buyValue = 0;
-        #endregion 
 
-        #region PROPERTIES
-        public string Name { get; }
-        public double PastProfit { get; private set; }
-        public double RetailValue { get; private set; }
-        public double CurrentProfit { get; private set; }
-        public double PurchaseValue { get; private set; }
+        /// <summary>
+        /// Tracks whether the trader has bought a product.
+        /// </summary>
+        private bool _hasBought = false;
+
+        /// <summary>
+        /// Stores the value at which the product was bought.
+        /// </summary>
+        private double _buyValue = 0;
+
         #endregion
 
-        #region CONSTRUCTORS
+        #region PROPERTIES
+
+        /// <summary>
+        /// Gets the name of the trader.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the total profit made by the trader in the past, excluding the current profit.
+        /// </summary>
+        public double PastProfit { get; private set; }
+
+        /// <summary>
+        /// Gets the current retail value of the product the trader is holding.
+        /// </summary>
+        public double RetailValue { get; private set; }
+
+        /// <summary>
+        /// Gets the current profit of the trader, considering both past profits and the difference between the buying and selling prices.
+        /// </summary>
+        public double CurrentProfit { get; private set; }
+
+        /// <summary>
+        /// Gets the value at which the product was bought.
+        /// </summary>
+        public double PurchaseValue { get; private set; }
+
+        #endregion
+
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Trader"/> class.
+        /// </summary>
+        /// <param name="name">The name of the trader.</param>
+        /// <param name="purchaseValue">The initial purchase value of the product.</param>
+        /// <param name="retailValue">The retail value of the product.</param>
         public Trader(string name , double purchaseValue , double retailValue)
         {
                 Name = name;
                 PurchaseValue = purchaseValue;
                 RetailValue = retailValue;
         }
+
         #endregion
 
         #region METHODS
+
+        /// <summary>
+        /// Updates the trader's state based on the latest product information.
+        /// </summary>
+        /// <param name="sender">The object that sent the event.</param>
+        /// <param name="e">The event arguments containing the product information.</param>
         public void UpdateProduct(object sender , EventArgs e)
         {
                 if (e is ProductEventArgs product)
@@ -40,6 +84,9 @@ public class Trader : ITrader
                 }
         }
 
+        /// <summary>
+        /// Prints the current profit of the trader, colored green for positive profit, red for negative profit, and white otherwise.
+        /// </summary>
         private void PrintProfit()
         {
                 if (CurrentProfit > 0)
@@ -51,12 +98,20 @@ public class Trader : ITrader
                 Console.ForegroundColor = ConsoleColor.White;
         }
 
+        /// <summary>
+        /// Simulates buying a product. Updates the trader's state accordingly.
+        /// </summary>
+        /// <param name="product">The product information received in the event arguments.</param>
         private void Buy(ProductEventArgs product)
         {
                 CurrentProfit = PastProfit;
                 CurrentProfit += _hasBought ? _buyValue - product.Value : 0;
         }
 
+        /// <summary>
+        /// Simulates holding a product. Updates the trader's state accordingly.
+        /// </summary>
+        /// <param name="product">The product information received in the event arguments.</param>
         private void Hold(ProductEventArgs product)
         {
                 _hasBought = true;
@@ -64,6 +119,10 @@ public class Trader : ITrader
                 CurrentProfit = PastProfit;
         }
 
+        /// <summary>
+        /// Simulates selling a product. Updates the trader's state accordingly.
+        /// </summary>
+        /// <param name="product">The product information received in the event arguments.</param>
         private void Sell(ProductEventArgs product)
         {
                 PastProfit += product.Value - _buyValue;
@@ -71,9 +130,16 @@ public class Trader : ITrader
                 _buyValue = 0.0;
                 CurrentProfit = PastProfit;
         }
+
         #endregion
 
         #region OVERRIDES
+
+        /// <summary>
+        /// Returns a string representation of the Trader object.
+        /// </summary>
+        /// <returns>A string containing the trader's name, current profit, purchase value, and retail value.</returns>
         public override string ToString() => $"{Name,-20} {CurrentProfit,10:f} EUR {PurchaseValue,10:f} EUR {RetailValue,10:f} EUR";
+
         #endregion
 }
